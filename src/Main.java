6067cfc9f19b5a97af8b9506ca8e48d14864a788
szt2otokov <szt2otokov@vasvari.org>
2023-09-14 11:10:40 +0200

    it works

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Main {
+    public static void Main(String[] args){
+        String nev = "Ótott-Kovács Tamás";
+        int kor = 18;
+        double magassag = 1.83;
+        boolean hazas = false;
+
+        System.out.println("My name: " + nev);
+        System.out.println("My age: " + kor);
+        System.out.println("My height: " + magassag);
+        System.out.println("Ami I married? " + hazas);
+    }
+}
+
