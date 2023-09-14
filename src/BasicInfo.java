e06337568fd17a2f3b3ab706302a47f95e06e81c
szt2otokov <szt2otokov@vasvari.org>
2023-09-14 11:36:39 +0200

    renamed main class + made it run

--- src/BasicInfo.java
+++ src/BasicInfo.java
@@ -1,7 +1,7 @@
 package src;
 
-public class Main {
-    public static void Main(String[] args){
+public class BasicInfo {
+    public static void main(String[] args){
         String nev = "Ótott-Kovács Tamás";
         int kor = 18;
         double magassag = 1.83;
